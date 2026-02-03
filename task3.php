86b4088242fc6f74d8b00291e4f781cdb4c8336b
Farishta Khan <farishtakhan1527@gmail.com>
2026-02-03 16:30:00 +0500

    Task3_DataTypes

---
+++ task3.php
@@ -0,0 +1,23 @@
+<?php
+
+$integer_variable = 15;
+$string_variable = "Farishta Khan";
+$float_variable = 3.27;
+$bool_variable = true;
+$boolean_variable = false;
+
+// var_dump  print variable TYPE + VALUE  + LENGTH OF THE STRING 
+var_dump($integer_variable);
+echo"<br>";
+var_dump($string_variable); 
+echo"<br>";
+var_dump($float_variable);
+echo"<br>";
+var_dump($bool_variable);
+echo"<br>";
+var_dump($boolean_variable);
+echo"<br>";
+
+
+
+?>
