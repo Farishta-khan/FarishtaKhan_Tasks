76f12d268f7d9b807649eb2dbe9e5565d2c9a4eb
Farishta Khan <farishtakhan1527@gmail.com>
2026-02-03 21:29:21 +0500

    Task14_Inheritance

--- task12.php
+++ task12.php
@@ -4,10 +4,14 @@ class Student {
     public $age;
 }
 
+// Create object
 $student1 = new Student();
+
+// Assign values
 $student1->name = "Farishta Khan";
 $student1->age = 23;
 
+// Print data
 echo "Name: " . $student1->name . "<br>";
 echo "Age: " . $student1->age . "<br>";
 ?>
---
+++ task14.php
@@ -0,0 +1,21 @@
+<?php
+class Person {
+    public $name;
+    public $age;
+
+    // Constructor
+    public function __construct($name, $age) {
+        $this->name = $name;
+        $this->age = $age;
+    }
+}
+
+class Student extends Person {
+    // Inherits everything from Person
+}
+
+$student1 = new Student("Sara Khan", 23);
+
+echo "Name: " . $student1->name . "<br>";
+echo "Age: " . $student1->age . "<br>";
+?>
